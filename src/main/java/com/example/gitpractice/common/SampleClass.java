d5ecac50b7f7dc5987ca12deda5f12d4dc946893
keno0403 <keno0403@estsecurity.com>
2022-09-18 17:11:41 +0900

    Update SampleClass

--- src/main/java/com/example/gitpractice/common/SampleClass.java
+++ src/main/java/com/example/gitpractice/common/SampleClass.java
@@ -3,5 +3,6 @@ package com.example.gitpractice.common;
 public class SampleClass {
     public void methodB() {
         System.out.println("hello world");
+        System.out.println("Hello there");
     }
 }
